58153194f2f9198abe2bf9dca67cf35dd02e966e
Enzo Moura <mouraenzo21@gmail.com>
2024-09-07 19:04:16 -0300

    Adicionado cadastro de aluno

--- main.c
+++ main.c
@@ -6,7 +6,9 @@ int main(){
     SalaDeAula * sala = saladeaula_cria();
     if (sala != NULL){
       printf("Sala De Aula criada!\n");
+        for (int i = 0; i < 3; i++){
         cadastraAluno(sala);
+        }
       return 0;
       }
     printf("Sala De Aula não foi criada :(\n");
