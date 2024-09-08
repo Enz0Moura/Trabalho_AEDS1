ffbfaf9f3628f354aceffaae6323123cf53d4f4f
Enzo Moura <mouraenzo21@gmail.com>
2024-09-07 21:26:56 -0300

    Adicionada função que remove '\n' do final da string.

--- utils/utils.h
+++ utils/utils.h
@@ -5,10 +5,15 @@
 #ifndef UTILS_H
 #define UTILS_H
 #include <stdio.h>
+#include <string.h>
 
 void flush_in() {
     char c;
     while ((c = getchar()) != '\n' && c != EOF);
+}
+
+void remove_newline(char *str) {
+    str[strcspn(str, "\n")] = '\0';
 }
 
 #endif //UTILS_H
